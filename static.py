70ca2367b543c5d00d929146bbf3dc0763a617cb
koumakpet <koumakpet@gmail.com>
2019-10-26 02:15:54 +0200

    Added docstrings for static

--- static.py
+++ static.py
@@ -1,22 +1,35 @@
 import os
-from pathlib import Path
 
 
 class GetOutOfLoop(Exception):
     pass
 
 
-def ensure_dir(file_path, LOG_FILE='logs/logfile.log'):
+def ensure_dir(file_path):
+    '''Make sure specified directory exists
+
+    Arguments:
+        file_path {string} -- Path for the directory
+
+    Returns:
+        bool -- Directory Created
+    '''
     directory = os.path.dirname(file_path)
     if not os.path.exists(directory):
         os.mkdir(directory)
-
-    if not Path(file_path).is_file():
-        with open(LOG_FILE, 'w'):
-            pass
+        return True
+    return False
 
 
 def is_number(num):
+    '''Check if specified string is number
+
+    Arguments:
+        num {string} -- String to check
+
+    Returns:
+        bool -- Is Number
+    '''
     try:
         _ = int(num)
         return True
